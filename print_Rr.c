0c75f34768a3bbeb680c1b77a2a5fe5c2f45503d
ZekariasBehailu <zeking02@gmail.com>
2022-08-09 16:52:50 +0300

    modified rot and rev funcs

--- print_Rr.c
+++ print_Rr.c
@@ -1,6 +1,33 @@
 #include "main.h"
 
 /**
+ * print_rev - prints string in reverse
+ * @ap: string
+ * @params: the parameters struct
+ *
+ * Return: number bytes printed
+ */
+
+int print_rev(va_list ap, params_t *params)
+{
+	int len, sum = 0;
+	char *str = va_arg(ap, char *);
+	(void)params;
+
+	if (str)
+	{
+		for (len = 0; *str; str++)
+			len++;
+		str--;
+		for (; len > 0; len--, str--)
+			sum += _putchar(*str);
+	}
+	return (sum);
+}
+
+#include "main.h"
+
+/**
  * print_rot13 - prints string in rot13
  * @ap: string
  * @params: the parameters struct
