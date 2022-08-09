20bb1141586840ae42956e20577e3cb555d8e4e6
Dcode3244 <dagimourgie111@gmail.com>
2022-08-09 14:32:44 +0300

    modified checker.c

--- checker.c
+++ checker.c
@@ -13,11 +13,17 @@ int (*checker(const char *c))(va_list ap, char *buffer)
 		{"s", print_string},
 		{"%", print_percent},
 		{"d", print_digit},
-		{"i", print_digit}
+		{"i", print_digit},
+		{"b", print_binary},
+		{"u", print_unsigned},
+		{"o", print_octal},
+		{"x", print_hexa},
+		{"X", print_X}
+
 	};
 	int i;
 
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < 10; i++)
 	{
 		if (*c == *(fmt[i].fmt))
 			return (fmt[i].print);
